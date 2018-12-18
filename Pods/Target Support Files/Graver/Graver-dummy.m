6a75342cb4b4f56e706731d0dd842116b59ab796
宋洋洋 <songyangyang03@meituan.com>
2018-12-18 12:54:05 +0800

    clean Pods

--- Pods/Target Support Files/Graver/Graver-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Graver : NSObject
-@end
-@implementation PodsDummy_Graver
-@end
--- Pods/Target Support Files/Pods-GraverDemoUITests/Pods-GraverDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GraverDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GraverDemoUITests
-@end
